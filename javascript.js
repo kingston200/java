78f3cdb0ba56c1f73c493da7d54911b28d5e418f
kingston200 <kingjames2009123@gmail.com>
2023-11-01 10:04:24 -0400

    java

--- javascript.js
+++ javascript.js
@@ -1,17 +1,7 @@
-function onClick(inputName) {
-  document.getElementById("output").innerHTML = "Welcome";
-  alert("Welcome, " + inputName + "!");
 
-  const myVariableName = { name: "Adam" };
-  console.log(myVariableName.name);
-
-  const myList = [];
-  console.log(myList[7]);
-}
-
-function otherButton(inputName2) {
-  document.getElementById("output2").innerHTML = "Accepted";
-  alert("Email accepted: " + inputName2);
+function otherButton(inputName) {
+  document.getElementById("output2").innerHTML = "HELLO";
+  alert("Email accepted: " + inputName);
 }
 
 
