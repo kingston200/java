e306132c5da53269f7a238b5e6dfca5f5ec24c68
kingston200 <kingjames2009123@gmail.com>
2023-11-01 09:10:14 -0400

    java

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function onClick(inputName) {
+    document.getElementById("output").innerHTML = "Welcome";
+    alert("Welcome, " + inputName + "!");
+  
+    const myVariableName = { name: "Adam" };
+    console.log(myVariableName.name);
+  
+    const myList = [];
+    console.log(myList[7]);
+  }
+  
+  function otherButton(inputName2) {
+    document.getElementById("output2").innerHTML = "Accepted";
+    alert("Email accepted: " + inputName2);
+  }
