955c6555c1e698f919c33e0d40483afcc1ab1cfe
kingston200 <kingjames2009123@gmail.com>
2023-11-01 10:19:41 -0400

    idk

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 const myList = [];
   console.log(myList[7]);
+
+
 function otherButton(inputName2) {
   document.getElementById("output2").innerHTML = "Accepted";
   alert("Email accepted: " + inputName2);
 }
-
 
 
 
