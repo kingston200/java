3ca4c546ea3ce955de7a5f5bbfc7380597498c5e
kingston200 <kingjames2009123@gmail.com>
2023-12-12 13:58:07 -0500

    class

--- javascript.js
+++ javascript.js
@@ -4,10 +4,13 @@ function onClick(inputValue) {
   $.get(url,function(data, status){
       if (status === 'success') {
           const pokemonNames = data.results.map(pokemon => {
+            const h2Tag = document.createElement('h2');
              return '<h2>' + pokemon.name + '<h2>';
           });   
           console.log(pokemonNames);
           document.getElementById("displayedText").innerHTML = pokemonNames;
+      } else {
+          console.log('error');
       }
   });
 
