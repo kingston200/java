3dd15859980ba4a9e5fe2fc7da839f2f8a0c8eae
kingston200 <kingjames2009123@gmail.com>
2024-01-09 13:47:32 -0500

    alot

--- javascript.js
+++ javascript.js
@@ -3,19 +3,19 @@ function onClick(inputValue) {
   const url = 'https://pokeapi.co/api/v2/pokemon';
   $.get(url,function(data, status){
       if (status === 'success') {
-          const pokemonNames = data.results.map(pokemon => {
-            const h2Tag = document.createElement('h2');
-             return '<h2>' + pokemon.name + '<h2>';
-          });   
-          console.log(pokemonNames);
-          document.getElementById("displayedText").innerHTML = pokemonNames;
+          addNamesToHTML(data);
       } else {
           console.log('error');
       }
   });
-
 }   
+function addNamesToHTML(data) {
+ const pokemonnames = data.results.map((pokemon) => {
+    const h2Element = document.createElement('h2');
+    h2Element.innerHTML = pokemon.name;
+    console.log("Name with H2 tag: ", h2Element);
+    return h2Element;
+  });
 
-function showNames(pokemon) {
-  return pokemon; 
+  $("#displayedText").html(pokemonnames);
 }
